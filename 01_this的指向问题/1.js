36ed0dcd44a15f6552cfe2773944933317dcbf35
weijiaxing <weijiaxing765@gmail.com>
2025-05-06 01:19:01 +0800

    array fun

--- 01_this的指向问题/1.js
+++ 01_this的指向问题/1.js
@@ -61,8 +61,65 @@
 // console.log('---')
 // console.log(bar);
 
+// function foo() {
+//   console.log('this: ',this)
+// }
+// foo()
+// foo.apply()
+
+// function foo(name, age, address) {
+//   console.log('foo:', this)
+//   console.log('参数：', name, age, address)
+// }
+// var obj = {name: 'why'}
+
+// var bar = foo.bind(obj, 'wei', 21)
+
+// console.log(foo())//foo:全局 ||参数： undefined undefined undefinedundefined
+
+// bar('zhengzhou')//foo: { name: 'why' } || 参数： wei 21 zhengzhou
+
+// function foo() {
+//   console.log('this name:',this.name)
+// }
+// var obj1 = {
+//   name: 'why',
+//   foo: foo
+// }
+// var obj2 = {
+//   name: 'gao'
+// }
+// obj1.foo.apply(obj2)
+
+// var obj = {
+//   name: 'why',
+//   foo: function() {
+//     console.log('foo:',this.name)
+//   }
+// }
+// var obj1 = {
+//   name: 'gao'
+// }
+// const newObj =  new obj.foo()
+
+//打印 aaa 说明bind的优先级高，
+// 打印全局对象说明new的优先级高
+// function foo() {
+//   console.log('foo: ',foo)
+// }
+// var bindFn = foo.bind('aaa')
+// new bindFn() //全局对象 说明是new的优先级高
+
+//先绑定bind
 function foo() {
-  console.log('this: ',this)
+  console.log('this:',this.name)
 }
-foo()
-foo.apply()
+var bindFun = foo.bind({name: 'aaa'})
+bindFun.apply({name: 'bbb'})//aaa
+
+//先绑定apply
+function foo1() {
+  console.log('this:',this.name)
+}
+ foo1.apply({name: 'aaa'})
+var bindFun1 =  foo1.bind({name: 'bbb'})//aaa
