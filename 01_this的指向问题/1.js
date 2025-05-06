1968803abb3f747214dca59fc6f133331cc1aba8
weijiaxing <weijiaxing765@gmail.com>
2025-05-06 12:33:15 +0800

    this project is over

--- 01_this的指向问题/1.js
+++ 01_this的指向问题/1.js
@@ -111,15 +111,177 @@
 // new bindFn() //全局对象 说明是new的优先级高
 
 //先绑定bind
-function foo() {
-  console.log('this:',this.name)
-}
-var bindFun = foo.bind({name: 'aaa'})
-bindFun.apply({name: 'bbb'})//aaa
+// function foo() {
+//   console.log('this:',this.name)
+// }
+// var bindFun = foo.bind({name: 'aaa'})
+// bindFun.apply({name: 'bbb'})//aaa
 
-//先绑定apply
-function foo1() {
-  console.log('this:',this.name)
+// //先绑定apply
+// function foo1() {
+//   console.log('this:',this.name)
+// }
+//  foo1.apply({name: 'aaa'})
+// var bindFun1 =  foo1.bind({name: 'bbb'})//aaa
+
+// var obj = {
+//   name: "obj",
+//   foo: () => {
+//     var bar = () => {
+//       console.log("bar fun this:", this);
+//     };
+//     return bar;
+//   },
+// };
+// var fn = obj.foo();
+// fn.apply({ name: "gao" });
+
+// var obj = {
+//   name: "obj",
+//   foo: function() {
+//     // var _this = this
+//     var bar = function() {
+//       console.log("bar fun this:", this);
+//     };
+//     return bar;
+//   },
+// };
+// var fn = obj.foo();
+// fn.apply({ name: "gao" });
+// //bar fun this: { name: 'gao' }
+
+// var obj = {
+//   name: "obj",
+//   foo: function() {
+//     var _this = this
+//     var bar = function() {
+//       console.log("bar fun this:", _this);
+//     };
+//     return bar;
+//   },
+// };
+// var fn = obj.foo();
+// fn.apply({ name: "gao" });
+//bar fun this: { name: 'obj', foo: [Function: foo] }
+
+// var name = 'window'
+
+// var person = {
+//   name: 'person',
+//   sayName: function() {
+//     console.log(this.name)
+//   }
+// }
+// function sayName() {
+//   var sss = person.sayName;
+//   sss()//默认绑定  window -> window
+//   person.sayName()//隐式绑定 person -> person
+//   (person.sayName)()//隐式绑定 person -> person
+//   (b = person.sayName)()//术语：间接函数引入 window -> window
+// }
+// sayName()
+
+// var name = "window";
+
+// var person1 = {
+//   name: "person1",
+//   foo1: function () {
+//     console.log(this.name);
+//   },
+//   foo2: () => console.log(this.name),
+//   foo3: function () {
+//     return function () {
+//       console.log(this.name);
+//     };
+//   },
+//   foo4: function () {
+//     return () => {
+//       console.log(this.name);
+//     };
+//   },
+// };
+
+// var person2 = {name: 'person2'}
+
+// person1.foo1() //隐式绑定 person1
+// person1.foo1.call(person2)//显式绑定 person2
+
+// person1.foo2()//上层作用域 window
+// person1.foo2.call(person2)//上层作用域 window
+
+// person1.foo3()()//默认绑定 window
+// person1.foo3.call(person2)()//默认绑定 window
+// person1.foo3().call(person2)//显式绑定 person2
+
+// person1.foo4()()// person1
+// person1.foo4.call(person2)()// person2
+// person1.foo4().call(person2)// person1
+
+// var name = "window";
+
+// function Person(name) {
+//   (this.name = name),
+//     (this.foo1 = function () {
+//       console.log(this.name);
+//     }),
+//     (this.foo2 = () => console.log(this.name)),
+//     (this.foo3 = function () {
+//       return function () {
+//         console.log(this.name);
+//       };
+//     }),
+//     (this.foo4 = function () {
+//       return () => {
+//         console.log(this.name);
+//       };
+//     });
+// }
+
+// var person1 = new Person("person1");
+// var person2 = new Person("person2");
+
+
+// person1.foo1() //隐式绑定 person1
+// person1.foo1.call(person2)//显式绑定 person2
+
+// person1.foo2()//上层作用域 person1
+// person1.foo2.call(person2)//上层作用域 person1
+
+// person1.foo3()()//默认绑定 window
+// person1.foo3.call(person2)()//默认绑定 window
+// person1.foo3().call(person2)//显式绑定 person2
+
+// person1.foo4()()// 上层作用域查找：person1（隐式绑定）
+// person1.foo4.call(person2)()//上层作用域查找：person2（显式绑定）
+// person1.foo4().call(person2)// 上层作用域查找：person1（隐式绑定）
+
+
+function Person(name){
+  this.name = name
+  this.obj = {
+    name: 'obj',
+    foo1: function() {
+      return function () {
+        console.log(this.name)
+      }
+    },
+    foo2: function() {
+      return ()=> {
+        console.log(this.name)
+      }
+    }
+  }
 }
- foo1.apply({name: 'aaa'})
-var bindFun1 =  foo1.bind({name: 'bbb'})//aaa
+
+var person1 = new Person('person1')
+var person2 = new Person('person2')
+
+person1.obj.foo1()()//默认绑定：window
+person1.obj.foo1.call(person2)()//默认绑定：window
+person1.obj.foo1().call(person2)//绑定：person2
+
+person1.obj.foo2()()//上层作用域查找：obj 隐式绑定
+person1.obj.foo2.call(person2)()//上层作用域查找：person2 显式绑定
+person1.obj.foo2().call(person2)//上层作用域查找：obj 隐式绑定
+
+
