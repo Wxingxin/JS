bc7866ae7f08d7b89122e5e715043617708c2c70
weijiaxing <weijiaxing765@gmail.com>
2025-05-07 22:51:54 +0800

    interator

--- 05_Promise/.js
+++ 05_Promise/.js
@@ -23,32 +23,60 @@
 //   }
 // );
 
-new Promise((resolve, reject) => {
-  resolve(999);
-}).then(
-  (successValue) => {
-    console.log(`successValue is :${successValue}`);
-  },
-  (errorReason) => {
-    console.log(`error :${errorReason}`);
-  }
+// new Promise((resolve, reject) => {
+//   resolve(999);
+// }).then(
+//   (successValue) => {
+//     console.log(`successValue is :${successValue}`);
+//   },
+//   (errorReason) => {
+//     console.log(`error :${errorReason}`);
+//   }
+// );
+
+// const promise = new Promise((resolve, reject) => {
+//   resolve(99);
+// });
+// promise
+//   .then(
+//     (successValue) => {
+//       console.log(`successValue is :${successValue}`);
+//     },
+//     (errorReason) => {
+//       console.log(`error :${errorReason}`);
+//     }
+//   )
+//   .then((val) => {
+//     console.log("val", val);
+//   })
+//   .then((val) => {
+//     console.log("val", val);
+//   });
+
+// //不使用promise resolve 的类方法
+// const promise_1 = new Promise((resolve, reject)=> {
+//   resolve(99)
+// })
+// promise_1.then(val=>
+//   console.log(val)
+// )
+
+// //使用promise resolve 的类方法
+// const promise_2 = Promise.resolve(99)
+// promise_2.then(val=> console.log(val))
+
+//不使用promise reject 的类方法
+const promise_1 = new Promise((resolve, reject) => {
+  reject(99);
+});
+promise_1.then(
+  (val) => console.log(val),
+  (err) => console.log(err)
 );
 
-const promise = new Promise((resolve, reject) => {
-  resolve(99);
-});
-promise
-  .then(
-    (successValue) => {
-      console.log(`successValue is :${successValue}`);
-    },
-    (errorReason) => {
-      console.log(`error :${errorReason}`);
-    }
-  )
-  .then((val) => {
-    console.log("val", val);
-  })
-  .then((val) => {
-    console.log("val", val);
-  });
+//使用promise reject 的类方法
+const promise_2 = Promise.reject(99);
+promise_2.then(
+  (val) => console.log(val),
+  (err) => console.log(err)
+);
